4884fd51434571add64c0d99dd98dcba0b6a98d5
Layan-Almuqrin <layanalmuqrin39@gmail.com>
2026-09-14 06:39:31 +0300

    Initial Commit

---
+++ Mulabbi/MulabbiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MulabbiApp.swift
+//  Mulabbi
+//
+//  Created by Layan Almuqrin on 03/04/1448 AH.
+//
+
+import SwiftUI
+
+@main
+struct MulabbiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
